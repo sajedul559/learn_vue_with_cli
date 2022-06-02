63730371e9caf389e165cbb4603300efd695e812
sajedul <98393471+sajedul43@users.noreply.github.com>
2022-06-02 11:50:56 +0600

    firs component render

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+
+      <h2>{{ title}}</h2>
+      <Navbar></Navbar>
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      title:'Huray our vue apps are running'
+    }
+  },
+  components:{
+    Navbar
+}
+  
+}
+</script>
+
+<style>
+h2{
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
