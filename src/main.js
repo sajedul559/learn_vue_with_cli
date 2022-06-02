f0d9861961eeefd57f2e6eebc046c89045eddacb
sajedul <98393471+sajedul43@users.noreply.github.com>
2022-06-02 16:20:32 +0600

    learn filter

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// Globaly filter created
+Vue.filter('snippet',val=>{
+  if(!val || typeof(val) != 'string') return ''
+  val = val.slice(0,80)
+  return val
+})
 new Vue({
   render: h => h(App),
 }).$mount('#app')
